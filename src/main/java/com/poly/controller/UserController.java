0dbb42979949e8e6bc6407a2ae292decc9535744
nguumiho <minhhoang25498@gmail.com>
2024-02-22 15:08:43 +0700

    Hoang add download excel from website

--- src/main/java/com/poly/controller/UserController.java
+++ src/main/java/com/poly/controller/UserController.java
@@ -4,9 +4,6 @@ import java.util.Map;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
